c6afb50eb297b67150b2bda3c31c7f5a4f7a6d5a
broisnischal <neeswebservice@gmail.com>
2024-07-22 01:21:37 +0545

    added-upstash-ratelimit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 
+import { Redis } from "@upstash/redis/cloudflare";
+import { Ratelimit } from "@upstash/ratelimit";
+
 type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key];
 };
@@ -28,6 +31,51 @@ app.get("/", (c) => {
 });
 
 app.get("/", (c) => c.text("Pretty Blog API"));
+
+app.get("/limit", async (c) => {
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(c.env),
+    limiter: Ratelimit.cachedFixedWindow(5, "5 s"),
+    enableProtection: true,
+    analytics: true,
+  });
+
+  const ip = c.req.header("X-Forwarded-For") ?? c.req.header("x-real-ip");
+
+  const identifier = ip ?? "global";
+
+  const { success, limit, remaining, reset } = await ratelimit.limit(
+    identifier
+  );
+
+  if (!ip) {
+    return c.json({
+      msg: "no ip",
+    });
+  }
+
+  if (!success) {
+    return c.json({
+      msg: "ratelimit",
+      identifier: identifier,
+      success: success,
+      ip: ip,
+      limit: limit,
+      remaining: remaining,
+      reset: reset,
+    });
+  }
+
+  return c.json({
+    msg: "ratelimit success",
+    ip: ip,
+    limit: limit,
+    success: success,
+    remaining: remaining,
+    reset: reset,
+  });
+});
+
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
 export default app;
